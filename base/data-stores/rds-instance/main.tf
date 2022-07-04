e856a8c891ae23179927be8a2ce482b6534c26b7
Herman Wong <hwong@hermanwong.net>
2022-07-04 22:08:49 +0000

    add subnet group name param

--- base/data-stores/rds-instance/main.tf
+++ base/data-stores/rds-instance/main.tf
@@ -8,9 +8,10 @@ resource "aws_db_instance" "this" {
   password               = var.db_password
   skip_final_snapshot    = true
   vpc_security_group_ids = [var.security_group_id]
-  publicly_accessible = false
+  publicly_accessible    = false
+  db_subnet_group_name   = aws_db_subnet_group.this.name
 }
 resource "aws_db_subnet_group" "this" {
-  name       = "main"
+  name       = var.identifier_prefix
   subnet_ids = [var.private_subnet_id]
 }
