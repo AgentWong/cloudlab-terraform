9c15ff53ee881c08ba5edae62f7eafca513173d6
Herman Wong <hwong@hermanwong.net>
2022-07-04 04:33:06 +0000

    Use db_name

--- base/data-stores/rds-instance/main.tf
+++ base/data-stores/rds-instance/main.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "this" {
   engine              = var.engine
   allocated_storage   = var.storage
   instance_class      = var.instance_class
-  name                = var.db_name
+  db_name                = var.db_name
   username            = var.db_username
   password            = var.db_password
   skip_final_snapshot = true
