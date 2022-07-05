2f7085d643e76ba57b8b605f20008546c546d73b
Herman Wong <hwong@hermanwong.net>
2022-07-05 01:53:04 +0000

    Fix sec group

--- base/data-stores/rds-instance/main.tf
+++ base/data-stores/rds-instance/main.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "this" {
   username               = var.db_username
   password               = var.db_password
   skip_final_snapshot    = true
-  vpc_security_group_ids = [var.security_group_id]
+  vpc_security_group_ids = [aws_security_group.db.id]
   publicly_accessible    = false
   db_subnet_group_name   = aws_db_subnet_group.this.name
   availability_zone      = var.availability_zone
