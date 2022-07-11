7ce2e0e30a06956ca77952c9a7cd17875edbe84e
Herman Wong <hwong@hermanwong.net>
2022-07-11 02:05:25 +0000

    Fix variable

--- base/compute/ec2/main.tf
+++ base/compute/ec2/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "this" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   get_password_data      = var.get_password_data
-  vpc_security_group_ids = [var.security_group_ids]
+  vpc_security_group_ids = var.security_group_ids
   subnet_id              = var.subnet_id
   user_data              = var.user_data
   tags = {
